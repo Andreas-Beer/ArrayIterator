8dd9e3b0d7014c0bc5d3c0675ea769a4f03c30f2
Andreas Beer <toldeandreasbeer@gmx.de>
2017-09-04 20:08:08 +0200

    add jamine specs

---
+++ test/spec/ArrayIterator.spec.js
@@ -0,0 +1,99 @@
+/* global expect */
+
+describe('ArrayIterator', function () {
+
+  var a;
+
+  beforeEach(function () {
+    a = ArrayIterator([1,2]);
+  });
+  
+  describe('length', function () {
+    it('returns the length of the given array', function () {
+      expect(a.length).toBe(2);
+    });
+  });
+  
+  describe('get({index})', function () {
+    it('returns the item by index', function () {
+      expect(a.get(0)).toBe(1);
+      expect(a.get(1)).toBe(2);
+    });
+    
+    it('returns the first item if one over the last is requested', function () {
+      expect(a.get(2)).toBe(1);
+    });
+  });
+  
+  describe('curr()', function () {
+    it('returns the current item', function () {
+      expect(a.curr()).toBe(1);
+    });
+  });
+  
+  describe('next()', function () {
+    it('returns the next item', function () {
+      expect(a.next()).toBe(2);
+    });
+    
+    it('returns the first item if the current is the last item', function () {
+      a.next();
+      expect(a.next()).toBe(1);
+    });
+  });
+  
+  describe('prev()', function () {
+    it('returns prevItem', function () {
+      a.prev();
+      expect(a.prev()).toBe(1);
+    });
+    
+    it('returns the last item if the current is the first item', function () {
+      expect(a.prev()).toBe(2);
+    });
+  });
+  
+  describe('walk({steps})', function () {
+    
+    beforeEach(function () {
+      a = ArrayIterator([1,2,3,4,5]);
+    });
+    
+    it('returns the item +{steps} away', function () {
+      expect(a.walk(1)).toBe(2);
+      expect(a.walk(3)).toBe(5);
+    });
+    
+    it('returns the item -{steps} away', function () {
+      a.walk(3);
+      expect(a.walk(-2)).toBe(2);
+    });
+    
+    describe('cycle = true', function () {
+      it('cycles through down', function () {
+        expect(a.walk(5)).toBe(1);
+      });
+      
+      it('cycles through up', function () {
+        expect(a.walk(-1)).toBe(5);
+      });
+    });
+    
+    describe('cycle = false', function () {
+      
+      beforeEach(function () {
+        a = ArrayIterator([1,2,3,4,5], false);
+      });
+      
+      it('stops at the end', function () {
+        expect(a.walk(5)).not.toBeDefined();
+      });
+      
+      it('stops at the begin', function () {
+        expect(a.walk(-1)).not.toBeDefined();
+      });
+    });
+    
+  });
+  
+});
